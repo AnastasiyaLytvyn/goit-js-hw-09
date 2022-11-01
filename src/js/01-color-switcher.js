efb3b89fa5843626c552204934284243c3a5ff90
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2022-11-01 15:00:11 +0200

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+let getEl = selector => document.querySelector(selector);
+getEl('.start').addEventListener('click', startTimerId);
+getEl('.stop').addEventListener('click', stopTimerId);
 
+   function startTimerId() {
+    setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        console.log('Start');
+    }, 1000);
+   } 
+
+function stopTimerId() {
+    clearInterval(startTimerId)
+    console.log('Stop');
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
