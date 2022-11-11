1e4dc4a83359b8169f15c2a06c6aaf2d914aabb6
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2022-11-11 16:52:21 +0200

    add third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import Notiflix from 'notiflix';
+
+getEl = selector => document.querySelector(selector);
+getEl('.form').addEventListener('submit', onSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  let firstDelay = Number(getEl('input[name="delay"]'));
+  let stepDelay = Number(getEl('input[name="step"]'));
+  let amountDelay = Number(getEl('input[name="amount"]'));
+
+  for (let i = 1; i <= amountDelay; i += 1) {
+    createPromise(i, firstDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    firstDelay += stepDelay;
   }
 }
